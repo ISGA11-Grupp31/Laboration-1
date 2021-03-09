e1db384b332f08554905d7bdefc6882863ea97f2
garmilium <77836341+garmilium@users.noreply.github.com>
2021-03-09 21:39:07 +0100

    kommenterade

--- DiceWithCookies.php
+++ DiceWithCookies.php
@@ -15,67 +15,82 @@
 				include( "include/OneDice.php" );
 				include( "include/SixDices.php" );
 
-				$disabled = false;
+				/* Variablen "disabled" deklareras och initieras till true och används för att avgöra om knappar i formuläret ska ha attributet disabled eller inte. */
+				$disabled = true;
 
 				//1
+				/* Nedanstående if-sats exekveras när sumbitknappen "btnNewGame" trycks och skapar två kakor med livslängd på en timme, eller om de redan finns, ändrar dess parametrar. */
 				if(isset($_POST["btnNewGame"]) ) {
-					echo("<p>New game!</p>");
-					setcookie("nbrOfRounds", 0, time() + 3600);
-					setcookie("sumOfAllrounds", 0, time() + 3600);
-					$disabled = false;
+					echo("<p>New game!</p>"); //Skriver ut strängen med html-kod.
+					setcookie("nbrOfRounds", "0", time() + 3600); // Kaka som används för att räkna antalet gånger tärningar rullats.
+					setcookie("sumOfAllrounds", "0", time() + 3600);// Kaka för att summera den totala summan av alla tärningskast.
+					$disabled = false; // Gör knapparna "btnRoll" och "btnExit" i formuläret användbara.
 				}
 
 				//2
-				if(!isset($_POST["btnRoll"]) && !isset($_POST["btnNewGame"]) && !isset($_POST["btnExit"]) && isset($_COOKIE["nbrOfRounds"]) && isset($_COOKIE["sumOfAllrounds"])) {
-					echo( "<p>Antal: ".$_COOKIE["nbrOfRounds"]."</p>");
+				/* Om sidan laddas utan att någon av formulär-knapparna är tryckta men kakorna finns på klienten körs följande kod */
+				if(!isset($_POST["btnRoll"]) && !isset($_POST["btnNewGame"]) && !isset($_POST["btnExit"]) && isset($_COOKIE["nbrOfRounds"]) && isset($_COOKIE["sumOfAllrounds"]) ) {
+					echo( "<p>Antal: ".$_COOKIE["nbrOfRounds"]."</p>"); // Skriver ut värdet i kakan "nbrOfRounds".
 					echo ( "<p>Totalsumma: ".$_COOKIE["sumOfAllrounds"]."</p>");
-					$medel = $_COOKIE["sumOfAllrounds"]/$_COOKIE["nbrOfRounds"];
+
+					/* Om någon av kakorna har värdet "0" körs förljande sats för att undvika division med noll. */
+					if($_COOKIE["nbrOfRounds"] === "0" || $_COOKIE["sumOfAllrounds"] === "0" ) {
+						$medel = 0; // $medel tilldelas värdet 0.
+					}
+					else {
+						$medel = $_COOKIE["sumOfAllrounds"]/$_COOKIE["nbrOfRounds"]; // Medelvärdet av alla tärningskast beräknas och placeras i $medel.
+					}
 					echo ( "<p>Medel: ".$medel."</p>");
-					//När cookievärdena är 0 och sidan laddas om blir det division med 0 och Internal Server Error.
+					$disabled = false;
+
 				}
 
 				//3
+				/* Om knappen "btnRoll" trycks och kakorna är satta körs följande kod. */
 				if( isset($_POST["btnRoll"]) && isset($_COOKIE["nbrOfRounds"]) && isset($_COOKIE["sumOfAllrounds"]) ) {
-					$oSixDices = new SixDices();
-					$oSixDices->rollDices();
-					echo( $oSixDices->svgDices() );
+					$oSixDices = new SixDices(); // Ett objekt med referensen $oSixDices skapas av klassen "SixDices" och dess konstruktor körs som skapar en vektor av dess attribut "$sixDices".
+					$oSixDices->rollDices(); // Funktionen "rollDices()" körs som simulerar 6 tärningskast genom att placera 6 objekt av OneDice-typ i vektorn $sixDices.
+					echo( $oSixDices->svgDices() ); // svgDices() returnerar en sträng innehållande en div som illustrerar de sex tärningsobjekten mot en grön bakgrund.
 
-					$nbr = $_COOKIE["nbrOfRounds"] + 1;
+					$nbr = $_COOKIE["nbrOfRounds"] + 1; // Antalet gånger "btnRoll" har tryckts + 1 placeras i $nbr.
 					echo( "<p>Antal: ".$nbr."</p>");
 
 					$sum = $_COOKIE["sumOfAllrounds"];
-					$sum +=$oSixDices->sumDices();
+					$sum +=$oSixDices->sumDices(); // Summan av alla tidigare tärningskast + värdena i $sixDices[] placeras i $sum.
 					echo ( "<p>Totalsumma: ".$sum."</p>");
 
 					$medel = $sum/$nbr;
 					echo ( "<p>Medel: ".$medel."</p>");
 
+					// Kakorna värden tilldelas $nbr respektive $sum och deras livslängd ställs om.
 					setcookie("nbrOfRounds", $nbr, time() + 3600);
 					setcookie("sumOfAllrounds", $sum, time() + 3600);
+
+					$disabled = false;
 				}
 
 				//4
+				/* Om "btnExit" trycks och kakorna finns på servern tas de bort. */
 				if( isset($_POST["btnExit"]) && isset($_COOKIE["nbrOfRounds"]) && isset($_COOKIE["sumOfAllrounds"]) ) {
 					setcookie("nbrOfRounds", "", time() - 3600);
 					setcookie("sumOfAllrounds", "", time() - 3600);
-					$disabled = true;
+
 				}
 
 				//5
-				if( !isset($_COOKIE["nbrOfRounds"]) && !isset($_COOKIE["sumOfAllrounds"])) {
+				/*if( !isset($_COOKIE["nbrOfRounds"]) && !isset($_COOKIE["sumOfAllrounds"])) {
 					$disabled = true;
-				}
-				//Var uppmärksam på att PHP-tolken används på ett flertal ställen i filen!
+				}*/
 			?>
 		</div>
 
 		<form action="<?php echo( $_SERVER["PHP_SELF"] ); ?>" method="post">
-		 <!-- $_SERVER["PHP_SELF"] returnerar filnamnet och sökvägen dit...  -->
+		 <!-- $_SERVER["PHP_SELF"] returnerar filnamnet och sökvägen dit.  -->
 			<input type="submit" name="btnRoll" class="btn btn-primary" value="Roll six dices" <?php if( $disabled ) { echo( "disabled" ); } ?>/>
 			<input type="submit" name="btnNewGame" class="btn btn-primary" value="New Game" />
 			<input type="submit" name="btnExit" class="btn btn-primary" value="Exit" <?php if( $disabled ) { echo( "disabled" ); } ?>/>
+			<!-- if( $disabled ) { echo( "disabled" ); } gör knappen oklickbar när $disabled är satt till true -->
 		</form>
-		<!-- if( $disabled ) { echo( "disabled" ); } gör knappen oklickbar när $disabled är satt till true -->
 
 		<script src="script/animation.js"></script>
 	</body>
--- DiceWithSession.php
+++ DiceWithSession.php
@@ -12,85 +12,97 @@
 
 		<div>
 			<?php
-			include( "include/OneDice.php" );
-			include( "include/SixDices.php" );
-			session_start();
+				include( "include/OneDice.php" );
+				include( "include/SixDices.php" );
+				session_start();
 
-			$disabled = false;
+				/* Variablen "disabled" deklareras och initieras till false och används för att avgöra om länkar ska ha bootstrap-klassen "disabled" eller inte. */
+				$disabled = false;
 
-			function deleteSession() {
+				function deleteSession() { // Tar bort sessionskakan och förstör datan kopplad till pågående session.
 
-				session_unset();
+					session_unset();
 
-				if( ini_get("session.use_cookies") ) {
+					if( ini_get("session.use_cookies") ) {
 
-					$sessionCookieData = session_get_cookie_params();
+						$sessionCookieData = session_get_cookie_params();
 
-					$path = $sessionCookieData["path"];
-					$domain = $sessionCookieData["domain"];
-					$secure = $sessionCookieData["secure"];
-					$httponly = $sessionCookieData["httponly"];
+						$path = $sessionCookieData["path"];
+						$domain = $sessionCookieData["domain"];
+						$secure = $sessionCookieData["secure"];
+						$httponly = $sessionCookieData["httponly"];
 
-					$name = session_name();
+						$name = session_name();
 
-					setcookie($name, "", time() - 3600, $path, $domain, $secure, $httponly);
+						setcookie($name, "", time() - 3600, $path, $domain, $secure, $httponly);
+					}
 
+					session_destroy();
 				}
 
-				session_destroy();
-			}
+				//1
+				/* Om query stringen "?linkNewGame" är satt skapas två sessionsvariabler. */
+				if(isset($_GET["linkNewGame"]) ) {
+					echo("<p>New game!</p>");
+					$_SESSION["nbrOfRounds"] = 0;
+					$_SESSION["sumOfAllrounds"] = 0;
+				}
 
-			//1
-			if(isset($_GET["linkNewGame"]) ) {
-				echo("<p>New game!</p>");
-				$_SESSION["nbrOfRounds"] = 0;
-				$_SESSION["sumOfAllrounds"] = 0;
-				//$disabled = false;
-			}
+				//2
+				/* Om länken "Exit" trycks och sessionsvariablerna är satta tas körs funktionen deleteSession(). */
+				if(isset($_GET["linkExit"]) && isset($_SESSION["nbrOfRounds"]) && isset($_SESSION["sumOfAllrounds"]) ) {
+					deleteSession();
+				}
 
-			//2
-			if(isset($_GET["linkExit"]) && isset($_SESSION["nbrOfRounds"]) && isset($_SESSION["sumOfAllrounds"]) ) {
-				deleteSession();
-			}
+				//3
+				/* Om man går in på sidan utan att ha någon tidigare session från sidan tas sessionen som skapas bort. */
+				if(!isset($_GET["linkExit"]) && !isset($_GET["linkNewGame"]) && !isset($_GET["linkRoll"]) && !isset($_SESSION["nbrOfRounds"]) && !isset($_SESSION["sumOfAllrounds"]) ) {
+					session_destroy();
+				}
 
-			//3
-			if(!isset($_GET["linkExit"]) && !isset($_GET["linkNewGame"]) && !isset($_GET["linkRoll"]) && !isset($_SESSION["nbrOfRounds"]) && !isset($_SESSION["sumOfAllrounds"]) ) {
-				session_destroy();
-			}
+				//4
+				/* Om ingen av länkarna är tryckta men sessionsvariablerna är satta körs följande kod. */
+				if(!isset($_GET["linkExit"]) && !isset($_GET["linkNewGame"]) && !isset($_GET["linkRoll"]) && isset($_SESSION["nbrOfRounds"]) && isset($_SESSION["sumOfAllrounds"]) ) {
+					echo( "<p>Antal: ".$_SESSION["nbrOfRounds"]."</p>"); // Skriver ut värdet i sessionsvariabeln "nbrOfRounds".
+					echo ( "<p>Totalsumma: ".$_SESSION["sumOfAllrounds"]."</p>");
 
-			//4
-			if(!isset($_GET["linkExit"]) && !isset($_GET["linkNewGame"]) && !isset($_GET["linkRoll"]) && isset($_SESSION["nbrOfRounds"]) && isset($_SESSION["sumOfAllrounds"]) ) {
-				echo( "<p>Antal: ".$_SESSION["nbrOfRounds"]."</p>");
-				echo ( "<p>Totalsumma: ".$_SESSION["sumOfAllrounds"]."</p>");
-				$medel = $_SESSION["sumOfAllrounds"]/$_SESSION["nbrOfRounds"];
-				echo ( "<p>Medel: ".$medel."</p>");
-				//Samma sak här, om cookievärdena är 0 och sidan laddas om blir det division med 0 och Internal Server Error.
-			}
+					/* Om någon av sessionsvariablerna har värdet 0 körs förljande sats för att undvika division med noll. */
+					if($_SESSION["nbrOfRounds"] === 0 || $_SESSION["sumOfAllrounds"] === 0 ) {
+						$medel = 0; // $medel tilldelas värdet 0.
+					}
+					else {
+						$medel = $_SESSION["sumOfAllrounds"]/$_SESSION["nbrOfRounds"]; // Medelvärdet av alla tärningskast beräknas och placeras i $medel.
+					}
+					echo ( "<p>Medel: ".$medel."</p>");
+				}
 
-			//5
-			if(isset($_GET["linkRoll"]) && isset($_SESSION["nbrOfRounds"]) && isset($_SESSION["sumOfAllrounds"]) ) {
-				$oSixDices = new SixDices();
-				$oSixDices->rollDices();
-				echo( $oSixDices->svgDices() );
+				//5
+				/* Om länken "Roll six dices" är trycks och sessionsvariablerna är satta körs följande kod. */
+				if(isset($_GET["linkRoll"]) && isset($_SESSION["nbrOfRounds"]) && isset($_SESSION["sumOfAllrounds"]) ) {
+					$oSixDices = new SixDices(); // Ett objekt med referensen $oSixDices skapas av klassen "SixDices" och dess konstruktor körs som skapar en vektor av dess attribut "$sixDices".
+					$oSixDices->rollDices(); // Funktionen "rollDices()" körs som simulerar 6 tärningskast genom att placera 6 objekt av OneDice-typ i vektorn $sixDices.
+					echo( $oSixDices->svgDices() ); // svgDices() returnerar en sträng innehållande en div som illustrerar de sex tärningsobjekten mot en grön bakgrund.
 
-				$nbr = $_SESSION["nbrOfRounds"];
-				$nbr = $nbr+1;
-				$_SESSION["nbrOfRounds"] = $nbr;
-				echo( "<p>Antal: ".$_SESSION["nbrOfRounds"]."</p>");
+					$nbr = $_SESSION["nbrOfRounds"] + 1; // Antalet gånger "Roll six dices" har tryckts + 1 placeras i $nbr.
+					echo( "<p>Antal: ".$nbr."</p>");
 
-				$sum = $_SESSION["sumOfAllrounds"];
-				$sum +=$oSixDices->sumDices();
-				$_SESSION["sumOfAllrounds"] = $sum;
-				echo ( "<p>Totalsumma: ".$_SESSION["sumOfAllrounds"]."</p>");
+					$sum = $_SESSION["sumOfAllrounds"];
+					$sum +=$oSixDices->sumDices(); // Summan av alla tidigare tärningskast + värdena i $sixDices[] placeras i $sum.
+					echo ( "<p>Totalsumma: ".$sum."</p>");
 
-				$medel = $_SESSION["sumOfAllrounds"]/$_SESSION["nbrOfRounds"];
-				echo ( "<p>Medel: ".$medel."</p>");
-			}
+					$medel = $sum/$nbr;
+					echo ( "<p>Medel: ".$medel."</p>");
 
-			//6
-			if(!isset($_SESSION["nbrOfRounds"]) && !isset($_SESSION["sumOfAllrounds"]) ) {
-				$disabled = true;
-			}
+					// Sessionsvariablerna får sina uppdaterade värden.
+					$_SESSION["nbrOfRounds"] = $nbr;
+					$_SESSION["sumOfAllrounds"] = $sum;
+				}
+
+				//6
+				/* Om sessionsvariablerna "nbrOfRounds" och "sumOfAllrounds" inte är satta får blir länkarna "Roll six dices" och "Exit" ej användbara. */
+				if(!isset($_SESSION["nbrOfRounds"]) && !isset($_SESSION["sumOfAllrounds"]) ) {
+					$disabled = true;
+				}
 
 			?>
 		</div>
@@ -98,7 +110,8 @@
 		<a href="<?php echo( $_SERVER["PHP_SELF"] );?>?linkRoll=true" class="btn btn-primary<?php if( $disabled ) { echo( " disabled" ); } ?>">Roll six dices</a>
 		<a href="<?php echo( $_SERVER["PHP_SELF"] );?>?linkNewGame=true" class="btn btn-primary">New game</a>
 		<a href="<?php echo( $_SERVER["PHP_SELF"] );?>?linkExit=true" class="btn btn-primary<?php if( $disabled ) { echo( " disabled" ); } ?>">Exit</a>
-
+		<!-- $_SERVER["PHP_SELF"] returnerar filnamnet och sökvägen dit.  -->
+		<!-- if( $disabled ) { echo( " disabled" ); } gör knappen oklickbar när $disabled är satt till true -->
 		<script src="script/animation.js"></script>
 
 	</body>
